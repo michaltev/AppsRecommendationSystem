c2ad2861c35c0d6a4c9e3b44266e7ec4e285a386
michaltev <michaltev@gmail.com>
2020-05-27 18:59:16 +0300

    cors & Mongo added

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+var app = express();
+app.use(cors());
+app.use(express.json());
+const port = 3000;
+
+const MongoClient = require('mongodb').MongoClient;
+
+app.listen(port, () => {
+	console.log('app is running on port '+port);
+});
+
 console.log("hello");
