d9d335e6d4e850e15b6e1f3c822286d1ec878a80
michaltev <michaltev@gmail.com>
2020-05-28 22:19:49 +0300

    get categories added

--- Server/server.js
+++ Server/server.js
@@ -7,8 +7,11 @@ app.use(express.json());
 const port = 3000;
 
 const appsController = require('./controllers/appsController');
+const categoriesController = require('./controllers/categoriesController');
 
 app.post('/filter/apps', (req, res) => { appsController.handleFilter(req, res) });
+
+app.get('/categories', (req, res) => { categoriesController.getCategories(req, res) })
 
 
 app.listen(port, () => {
