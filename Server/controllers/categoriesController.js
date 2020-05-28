1bfe6931d85ceddfc7a8d464eef4b73e8b7d26cf
michaltev <michaltev@gmail.com>
2020-05-28 18:11:22 +0300

    hardcoded controllers

---
+++ Server/controllers/categoriesController.js
@@ -0,0 +1,37 @@
+const _lstCategories = [
+    "House And Home",
+    "Maps And Navigation",
+    "Health And Fitness",
+    "Travel And Local",
+    "Music And Audio",
+    "Communication",
+    "Tools",
+    "Entertainment",
+    "News And Magazines",
+    "Shopping",
+    "Events",
+    "Dating",
+    "Lifestyle",
+    "Books And Reference",
+    "Weather",
+    "Business",
+    "Finance",
+    "Social",
+    "Photography",
+    "Food And Drink",
+    "Video Players",
+    "Productivity",
+    "Parenting",
+    "Education",
+    "Auto And Vehicles",
+    "Medical",
+    "Sports"
+];
+
+const getCategories = (req, res) => {
+	res.json(_lstCategories);
+};
+
+module.exports = {
+	getCategories: getCategories
+}
--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 const port = 3000;
 
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 
 app.listen(port, () => {
 	console.log('app is running on port '+port);
